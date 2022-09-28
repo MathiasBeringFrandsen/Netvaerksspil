053df184d8a79a2d68a87c5d1cf07a774f11f048
Mathias <tidspe@gmail.com>
2022-09-28 11:04:59 +0200

    Hello World

--- theGame/src/Game2022/game/ClientThread.java
+++ theGame/src/Game2022/game/ClientThread.java
@@ -1,6 +1,9 @@
 package Game2022.game;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ClientThread extends Thread {
     private BufferedReader input;
@@ -11,9 +14,22 @@ public class ClientThread extends Thread {
 
     public void run(){
         while (true){
+            try {
+                String arrayString;
+                arrayString = input.readLine();
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    public static void sendPersoner(ArrayList<Person> personer, DataOutputStream outstream) throws IOException {
+        String persons = "";
+        for (int i = 0; i< personer.size(); i++){
+            persons = persons + personer.get(i).getId()+ " " + personer.get(i).getNavn() + " " + personer.get(i).getBy() + "#";
+        }
+        outstream.writeBytes(persons);
+    }
 
 }
--- theGame/src/Game2022/game/ServerThread.java
+++ theGame/src/Game2022/game/ServerThread.java
@@ -3,6 +3,8 @@ import javafx.application.Application;
 
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+
 public class ServerThread extends Thread{
 	Socket connSocket;
 	common c;
@@ -16,7 +18,10 @@ public class ServerThread extends Thread{
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
 			String newPlayerName = inFromClient.readLine();
 			GameLogic.makePlayers(newPlayerName);
+			//return something;
+			while (true){
 
+			}
 			
 			// Do the work and the communication with the client here	
 			// The following two lines are only an example
@@ -25,5 +30,13 @@ public class ServerThread extends Thread{
 			e.printStackTrace();
 		}		
 		// do the work here
+	}
+	public static ArrayList<Person> modtagPersoner(ArrayList<Person> personer, BufferedReader instream) throws IOException {
+		String[] personArray = instream.readLine().split("#");
+		for (int i = 0; i < personArray.length; i++) {
+			String[] thisPerson = personArray[i].split(" ");
+			personer.add(new Person(Integer.parseInt(thisPerson[0]), thisPerson[1], thisPerson[2]));
+		}
+		return personer;
 	}
 }
